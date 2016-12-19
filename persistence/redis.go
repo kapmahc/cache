a0167be82b7887402f1bf3e51ea6cc8ef9a8c97c
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-19 11:21:01 -0800

    support select db

--- persistence/redis.go
+++ persistence/redis.go
@@ -13,6 +13,35 @@ type RedisStore struct {
 	defaultExpiration time.Duration
 }
 
+// NewRedisCacheWithDb returns a RedisStore
+// until redigo supports sharding/clustering, only one host will be in hostList
+func NewRedisCacheWithDb(host string, db string, defaultExpiration time.Duration) *RedisStore {
+	var pool = &redis.Pool{
+		MaxIdle:     5,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			// the redis protocol should probably be made sett-able
+			c, err := redis.Dial("tcp", host)
+			if err != nil {
+				return nil, err
+			}
+			if _, err := c.Do("SELECT", db); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, err
+		},
+		// custom connection test method
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if _, err := c.Do("PING"); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	return &RedisStore{pool, defaultExpiration}
+}
+
 // NewRedisCache returns a RedisStore
 // until redigo supports sharding/clustering, only one host will be in hostList
 func NewRedisCache(host string, password string, defaultExpiration time.Duration) *RedisStore {
